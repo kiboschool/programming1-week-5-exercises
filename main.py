d6f680e34fc3105223eb3a7034c33b84869cacb4
Rob Cobb <rwcobbjr@gmail.com>
2022-09-03 00:02:36 -0600

    add solution

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# Write your functions here
-# See the instructions in the tab to the right
+def km_to_miles(value):
+  return value * 0.621371
 
+def miles_to_km(value):
+  return value * 1.60934
 
+def lbs_to_kg(value):
+  return value * 0.453592
+
+def kg_to_lbs(value):
+  return value * 2.20462
