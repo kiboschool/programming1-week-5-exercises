bcce39bfad41dd6b5e4c02f13d3bbb710db626c1
David Walter <david@kibo.school>
2023-06-13 16:47:42 -0700

    Change approach to the autograder

--- gradescope/make_zip.sh
+++ gradescope/make_zip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Loop through all subdirectories to find test files
-test_cases=$(find .. -type f -name "test_*.py" -maxdepth 2)
+test_cases=$(find .. -type f -name "test*.py" -maxdepth 2)
 for test in $test_cases; do
   # Get the parent directory name
   parent_dir=$(basename "$(dirname "$test")")
@@ -9,7 +9,7 @@ for test in $test_cases; do
   cp "${test}" "${test#*/}"
 done
 
-find . -type f -name 'test_*.py' -exec zip gradescope.zip setup.sh requirements.txt run_autograder run_tests.py {} +
+find . -type f -name 'test*.py' -exec zip gradescope.zip setup.sh requirements.txt run_autograder run_tests.py merge_results.py {} +
 
 # Remove test files from gradescope after zip
 for test in $test_cases; do
